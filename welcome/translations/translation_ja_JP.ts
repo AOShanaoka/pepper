61f8661b981f4d442c821b8a704706afc8aa5ed2
thanaoka <t.hanaoka@aos.com>
2020-03-27 13:40:53 +0900

    add new file

---
+++ welcome/translations/translation_ja_JP.ts
@@ -0,0 +1,102 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ja_JP">
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>いらっしゃいませ。AOSデータへようこそ。</source>
+            <comment>Text</comment>
+            <translation type="obsolete">いらっしゃいませ。AOSデータへようこそ。</translation>
+        </message>
+        <message>
+            <source>いらっしゃいませ。</source>
+            <comment>Text</comment>
+            <translation type="obsolete">いらっしゃいませ。</translation>
+        </message>
+        <message>
+            <source>いらっしゃいませッ。AOSデータへようこそッ。</source>
+            <comment>Text</comment>
+            <translation type="obsolete">いらっしゃいませッ。AOSデータへようこそッ。</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>いらっしゃいませッ。｜AOSデータへようこそッ。</source>
+            <comment>Text</comment>
+            <translation type="unfinished">いらっしゃいませッ。｜AOSデータへようこそッ。</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <source>こんにちは</source>
+            <comment>Text</comment>
+            <translation type="vanished">こんにちは</translation>
+        </message>
+        <message>
+            <source>AOSデータへようこそ。</source>
+            <comment>Text</comment>
+            <translation type="obsolete">AOSデータへようこそ。</translation>
+        </message>
+        <message>
+            <source>受付用紙を記入して、机の上の電話でご用件をお伝えください。</source>
+            <comment>Text</comment>
+            <translation type="obsolete">受付用紙を記入して、机の上の電話でご用件をお伝えください。</translation>
+        </message>
+        <message>
+            <source>受付用紙を記入して、机の上の電話でご用件をオツタエください。</source>
+            <comment>Text</comment>
+            <translation type="obsolete">受付用紙を記入して、机の上の電話でご用件をオツタエください。</translation>
+        </message>
+        <message>
+            <source>受付用紙を記入して、机の上の電話でご用件をオつたえください。</source>
+            <comment>Text</comment>
+            <translation type="obsolete">受付用紙を記入して、机の上の電話でご用件をオつたえください。</translation>
+        </message>
+        <message>
+            <source>受付用紙を記入して、机の上の電話でご用件ヲおつたえください。</source>
+            <comment>Text</comment>
+            <translation type="obsolete">受付用紙を記入して、机の上の電話でご用件ヲおつたえください。</translation>
+        </message>
+        <message>
+            <source>受付用紙を記入して、机の上の電話で、ごヨウ件ヲおつたえください。</source>
+            <comment>Text</comment>
+            <translation type="obsolete">受付用紙を記入して、机の上の電話で、ごヨウ件ヲおつたえください。</translation>
+        </message>
+        <message>
+            <source>受付用紙を記入して、机の上の電話で、ごヨう件ヲおつたえください。</source>
+            <comment>Text</comment>
+            <translation type="obsolete">受付用紙を記入して、机の上の電話で、ごヨう件ヲおつたえください。</translation>
+        </message>
+        <message>
+            <source>受付用紙を記入して、机の上の電話で、ゴヨう件ヲおつたえください。</source>
+            <comment>Text</comment>
+            <translation type="obsolete">受付用紙を記入して、机の上の電話で、ゴヨう件ヲおつたえください。</translation>
+        </message>
+        <message>
+            <source>受付用紙を記入して、机の上の電話で、ごようけんヲおつたえください。</source>
+            <comment>Text</comment>
+            <translation type="obsolete">受付用紙を記入して、机の上の電話で、ごようけんヲおつたえください。</translation>
+        </message>
+        <message>
+            <source>受付用紙を記入して、机の上の電話で、ごようけんをおつたえください。</source>
+            <comment>Text</comment>
+            <translation type="obsolete">受付用紙を記入して、机の上の電話で、ごようけんをおつたえください。</translation>
+        </message>
+        <message>
+            <source>受付用紙を記入して、｜机の上の電話で、ごようけんをおつたえください。</source>
+            <comment>Text</comment>
+            <translation type="obsolete">受付用紙を記入して、｜机の上の電話で、ごようけんをおつたえください。</translation>
+        </message>
+        <message>
+            <source>受付用紙を記入して、｜机の上の電話で、ごよ鵜けんをおつたえください。</source>
+            <comment>Text</comment>
+            <translation type="obsolete">受付用紙を記入して、｜机の上の電話で、ごよ鵜けんをおつたえください。</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>受付用紙を記入して、｜机の上の電話で、ごよ鵜けンをおつたえください。</source>
+            <comment>Text</comment>
+            <translation type="unfinished">受付用紙を記入して、｜机の上の電話で、ごよ鵜けンをおつたえください。</translation>
+        </message>
+    </context>
+</TS>
